516cbe0fd7e3f6945e3fe2049d5c06d884c64c61
Dorian Tudorache <dorian.snaz@gmail.com>
2016-06-09 22:22:08 +0300

    first upload here

---
+++ 4_code/build/webpack.base.conf.js
@@ -0,0 +1,81 @@
+var path        = require('path'),
+    ap          = require('autoprefixer'),
+    short       = require('postcss-short'),
+    magic       = require('postcss-magic-animations'),
+    magician    = require('postcss-font-magician'),
+    pxtorem     = require('postcss-pxtorem');
+
+module.exports = {
+  entry: {
+    app:  './src/main.js'
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/dist/',
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.pug', '.scss'],
+    alias: {
+      'src': path.resolve(__dirname, '../src')
+    }
+  },
+  resolveLoader: {
+    root: path.join(__dirname, 'node_modules')
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.pug$/,
+        loader: 'pug-html-loader',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel!eslint',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'url',
+        query: {
+          limit: 10000,
+          name: '[name].[ext]?[hash:7]'
+        }
+      },
+      {
+        test: /\.scss$/,
+        loaders: 'sass'
+      },
+      { 
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+        loader: "url-loader?limit=10000&minetype=application/font-woff" 
+      },
+      { 
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+        loader: "file-loader" 
+      }
+    ]
+  },
+  'pug-html-loader': {
+    pretty: true
+  },
+  eslint: {
+    formatter: require('eslint-friendly-formatter')
+  },
+  postcss: function() {
+    return {
+      defaults: [
+        short,
+        magic({atRoot: true}),
+        pxtorem({replace: false}),
+        currency,
+        magician,
+        ap
+      ]
+    }
+  }
+}
