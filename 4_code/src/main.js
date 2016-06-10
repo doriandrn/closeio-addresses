9aff4796ada6a8288a2de5bf987a6aa5e9f8b0c1
Dorian Tudorache <dorian.snaz@gmail.com>
2016-06-10 22:05:14 +0300

    wurkinmap

--- 4_code/src/main.js
+++ 4_code/src/main.js
@@ -3,6 +3,9 @@ require('./style.scss');
 let 
 	placeSearch, 
 	autocomplete,
+	map,
+	marker,
+	infowindow,
 	componentForm = {
 	  street_number: 'short_name',
 	  route: 'long_name',
@@ -96,20 +99,38 @@ function initTags( el ) {
 }
 
 function initAutocomplete() {
-  // Create the autocomplete object, restricting the search to geographical
-  // location types.
   autocomplete = new google.maps.places.Autocomplete(
-      /** @type {!HTMLInputElement} */(document.getElementById('address')),
+      (document.getElementById('address')),
       {types: ['geocode']});
 
-  // When the user selects an address from the dropdown, populate the address
-  // fields in the form.
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -33.8688, lng: 151.2195},
+    zoom: 13
+  });
+
+  autocomplete.bindTo('bounds', map);
+
+  infowindow = new google.maps.InfoWindow();
+  marker = new google.maps.Marker({
+    map: map,
+    anchorPoint: new google.maps.Point(0, -29)
+  });
+  
   autocomplete.addListener('place_changed', fillInAddress);
 }
 
+
+
+
 function fillInAddress() {
+	window.dispatchEvent(new Event('resize'));
   // Get the place details from the autocomplete object.
   var place = autocomplete.getPlace();
+
+  if (!place.geometry) {
+    window.alert("Autocomplete's returned place contains no geometry");
+    return;
+  }
 
   for (var component in componentForm) {
     document.getElementById(component).value = '';
@@ -125,6 +146,35 @@ function fillInAddress() {
       document.getElementById(addressType).value = val;
     }
   }
+
+  if (place.geometry.viewport) {
+    map.fitBounds(place.geometry.viewport);
+  } else {
+    map.setCenter(place.geometry.location);
+    map.setZoom(17);  // Why 17? Because it looks good.
+  }
+
+  marker.setIcon(/** @type {google.maps.Icon} */({
+    url: place.icon,
+    size: new google.maps.Size(71, 71),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(17, 34),
+    scaledSize: new google.maps.Size(35, 35)
+  }));
+  marker.setPosition(place.geometry.location);
+  marker.setVisible(true);
+
+  var address = '';
+  if (place.address_components) {
+    address = [
+      (place.address_components[0] && place.address_components[0].short_name || ''),
+      (place.address_components[1] && place.address_components[1].short_name || ''),
+      (place.address_components[2] && place.address_components[2].short_name || '')
+    ].join(' ');
+  }
+
+  infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+  infowindow.open(map, marker);
 }
 
 // Bias the autocomplete object to the user's geographical location,
